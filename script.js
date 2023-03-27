cec57cd796233e4155e18209f3cdb006eff4bed6
Ganapathy77 <ganapathy14777@gmail.com>
2023-03-28 00:08:02 +0530

    Completed Coding

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+let arr = [1, 2, 3, 4, 5];
+const output = document.querySelector('#output');
+
+function manipulate(arr) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(arr);
+        }, 3000)
+    }).then((data) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                output.innerText = data.filter((element) => { return element % 2 != 0 })
+                resolve(data)
+            },1000)
+        })
+    }).then((data)=>{
+        return new Promise((resolve, reject)=>{
+            setTimeout(() => {
+                let evenArr = data.filter((element) => {return element %2 == 0})
+                evenArr = evenArr.map( element => element*2)
+                resolve(evenArr)
+            },2000)
+        })
+    })
+}
+
+manipulate(arr).then((data)=>{
+    output.innerText = data;
+}).catch((err)=>{
+    console.log('Error ...')
+})
